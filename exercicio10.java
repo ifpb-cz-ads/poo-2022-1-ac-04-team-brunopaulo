1666e80c6a447efd912f2f45645d78e0ead4200e
Brunoklb <brunola.nogueira@gmail.com>
2022-06-04 23:12:13 -0300

    comitando as 5 questões restantes

---
+++ exercicio10.java
@@ -0,0 +1,9 @@
+public class exercicio10 {
+    public static void main(String args[]) {
+        int x=10, i=0;
+        do{
+            System.out.println(x);
+            i++;
+        }while(i<20);
+    }
+}
---
+++ pratica4.java
@@ -0,0 +1,8 @@
+public class pratica4 {
+    public static void main(String args[]) {
+        int i;
+        for(i=0;i<=100;i++){
+            System.out.println(i);
+        }
+    }
+}
